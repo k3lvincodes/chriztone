0faa49aca8f28b790c1fea03f0805a23a27db928
Kelvin Feh <k3lvincodes@gmail.com>
2025-09-09 23:33:02 +0000

    "Hi! I'm Paul" should have like 20px added to it's original margin botto

--- src/components/home/default-view.tsx
+++ src/components/home/default-view.tsx
@@ -21,7 +21,7 @@ const DefaultView: React.FC<DefaultViewProps> = ({ onUpload }) => {
     <div className="grid md:grid-cols-2 gap-x-20 gap-y-16 items-start">
       <div className="space-y-10 md:pt-12">
         <div className="space-y-5">
-            <p className="text-3xl font-bold text-black">Hi! I'm Paul</p>
+            <p className="text-3xl font-bold text-black mb-5">Hi! I'm Paul</p>
             <h1 className="text-[50px] font-semibold !leading-tight">
                 <span className="text-[#9966FF]">UI</span><span className="text-[#00CC99]">/UX</span> <span className="text-black">Designer &amp;</span>
                 <br />
